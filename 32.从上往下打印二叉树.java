83f0061af13ec36184abd126ade980637607be71
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-04-29 16:08:14 +0800

    Update and rename my_32.java to 32.从上往下打印二叉树.java

---
+++ 32.从上往下打印二叉树.java
@@ -0,0 +1,39 @@
+import java.util.*;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+            while (cnt > 0) {
+                TreeNode node = queue.poll();
+                ans.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                --cnt;
+            }
+        }
+        return ans;
+    }
+}
