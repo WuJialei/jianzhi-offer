d93d6af0febc3a02b8c00b3c8881afda26ed4aec
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-05-11 13:33:42 +0800

    Update my_43.java

--- my_43.java
+++ my_43.java
@@ -1,7 +1,19 @@
-class Solution {
-public:
-    int NumberOf1Between1AndN_Solution(int n)
-    {
-    
+public class Solution {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i = i * 10) {
+            int high = n / (i * 10);
+            int low = n % i;
+            int cur = (n/i)%10;
+            if (cur == 0) {
+                cnt += high * i;
+            } else if (cur == 1) {
+                cnt += high * i + low + 1;
+            } else {
+                cnt += (high + 1) * i;
+            }
+        }
+        return cnt;
+        
     }
-};
+}
