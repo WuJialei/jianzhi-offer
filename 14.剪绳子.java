5786012b3c9926dc51d6dab98a2e53f74ee8f3ec
wujialei <42853741+WuJialei@users.noreply.github.com>
2020-06-23 11:13:09 +0800

    Update 14.剪绳子.java

--- 14.剪绳子.java
+++ 14.剪绳子.java
@@ -1,27 +1,20 @@
-public class Solution {
-    public int cutRope(int target) {
-        int len = target + 1;
-        int[] dp = new int[len];
-        if (target < 2) {
+class Solution {
+    public int cuttingRope(int n) {
+        if (n <= 1) {
             return 0;
-        } else if (target == 2) {
-            return 1;
-        } else if (target == 3) {
-            return 2;
         }
-        // dp[i]表示必须剪一下
-        dp[0] = 0;
-        dp[1] = 0;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
         dp[2] = 1;
-        dp[3] = 2;
-        for (int i = 4; i <= target; ++i) {
+        for (int i = 3; i <= n; ++i) {
             int max = 0;
             for (int j = 1; j <= i/2 + 1; ++j) {
-                max = Math.max(max, dp[j] * dp[i-j]);
-                max = Math.max(max, j * (i-j));
+                int left = Math.max(j, dp[j]);
+                int right = Math.max(i-j, dp[i-j]);
+                max = Math.max(left * right, max);
             }
             dp[i] = max;
         }
-        return dp[target];
+        return dp[n];
     }
 }
